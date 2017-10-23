3c22262cb08ed7c239ae4e4fbe1de93a0c643793
yellowpuki <32398419+yellowpuki@users.noreply.github.com>
2017-10-23 16:11:31 +0300

    Update Program.cs

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,17 +10,17 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 2;
-            char sym1 = '*';
-
-            Draw(x1, y1, sym1);
-
-            int x2 = 3;
-            int y2= 4;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+            
+            Point p2 = new Point();
+            p2.x = 1;
+            p2.y = 3;
+            p2.sym = '*';
+            p2.Draw();
 
             Console.ReadLine();
         }
